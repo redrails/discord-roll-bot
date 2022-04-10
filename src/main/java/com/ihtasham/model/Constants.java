9330df49ec709025c61a259702284fec5477eb18
redrails <redrailz@gmail.com>
2022-04-11 00:34:10 +0100

    change command

--- src/main/java/com/ihtasham/model/Constants.java
+++ src/main/java/com/ihtasham/model/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static final String MESSAGE_ID = "messageId";
     public static final char COMMAND_PREFIX = '!';
-    public static final String ROLL_COMMAND = "roll";
+    public static final String ROLL_COMMAND = "cs";
     public static final String CONFIG_FILE_NAME = "app.config";
 }
