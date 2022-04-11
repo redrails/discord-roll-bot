8566ce193d3a429702abe385d40ecf77e2fabfe9
redrails <redrailz@gmail.com>
2022-04-11 23:44:30 +0100

    change help command

--- src/main/java/com/ihtasham/model/Constants.java
+++ src/main/java/com/ihtasham/model/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String PLAYER_LIST = "playerList";
     public static final char COMMAND_PREFIX = '!';
     public static final String ROLL_COMMAND = "cs";
-    public static final String HELP_COMMAND = "help";
+    public static final String HELP_COMMAND = "cshelp";
     public static final String ADD_COMMAND = "add";
     public static final String TOKEN = "BOT_TOKEN";
     public static final String PLAYING = "!cs";
