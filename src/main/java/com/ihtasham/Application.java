e07562e56d20af17a8398b9b7949a297e9d3aea2
name <email>
2022-04-15 01:28:18 +0100

    Made some improvements and added guild ids

--- src/main/java/com/ihtasham/Application.java
+++ src/main/java/com/ihtasham/Application.java
@@ -3,14 +3,11 @@ package com.ihtasham;
 import com.ihtasham.Listeners.MessageListener;
 import com.ihtasham.Listeners.ReactionListener;
 import com.ihtasham.database.DBManager;
-import lombok.extern.slf4j.Slf4j;
 import com.ihtasham.model.Constants;
+import javax.security.auth.login.LoginException;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-
-import javax.security.auth.login.LoginException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @Slf4j
 public class Application {
--- src/main/java/com/ihtasham/database/DBManager.java
+++ src/main/java/com/ihtasham/database/DBManager.java
@@ -1,17 +1,17 @@
 package com.ihtasham.database;
 
+import static com.ihtasham.model.Constants.MESSAGE_ID;
+import static com.ihtasham.model.Constants.PLAYER_LIST;
+
+import java.util.concurrent.ConcurrentMap;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.Serializer;
 
-import java.util.concurrent.ConcurrentMap;
-
-import static com.ihtasham.model.Constants.MESSAGE_ID;
-import static com.ihtasham.model.Constants.PLAYER_LIST;
-
 public final class DBManager {
 
   final DB db;
+
   final ConcurrentMap<String, String> map;
 
   public DBManager() {
@@ -19,47 +19,51 @@ public final class DBManager {
     this.map = db.hashMap("map", Serializer.STRING, Serializer.STRING).createOrOpen();
   }
 
-  private void put(String key, String value) {
-    map.put(key, value);
+  private void put(final String guildId, final String key, final String value) {
+    map.put(guildId + key, value);
   }
 
-  private String get(String key) {
-    return map.get(key);
+  private String get(final String guildId, final String key) {
+    return map.get(guildId + key);
   }
 
-  private boolean containsKey(String key) {
-    return map.containsKey(key);
+  private boolean containsKey(final String guildId, final String key) {
+    return map.containsKey(guildId + key);
   }
 
-  private void removeKey(String key) {
-    map.remove(key);
+  private void removeKey(final String guildId, final String key) {
+    map.remove(guildId + key);
+  }
+
+  public void clearAllInGuild(final String guildId) {
+    map.keySet().stream().filter(k -> k.contains(guildId)).forEach(map::remove);
   }
 
   public void clearAll() {
     map.clear();
   }
 
-  public void putMessageId(String messageId) {
-    this.put(MESSAGE_ID, messageId);
+  public void putMessageId(final String guildId, final String messageId) {
+    this.put(guildId, MESSAGE_ID, messageId);
   }
 
-  public boolean messageExists() {
-    return this.containsKey(MESSAGE_ID);
+  public boolean messageExists(final String guildId) {
+    return this.containsKey(guildId, MESSAGE_ID);
   }
 
-  public boolean playersExists() {
-    return this.containsKey(PLAYER_LIST);
+  public boolean playersExists(final String guildId) {
+    return this.containsKey(guildId, PLAYER_LIST);
   }
 
-  public String getMessage() {
-    return this.get(MESSAGE_ID);
+  public String getMessage(final String guildId) {
+    return this.get(guildId, MESSAGE_ID);
   }
 
-  public void addPlayer(final String player) {
-    map.put(PLAYER_LIST, map.get(PLAYER_LIST) + "," + player);
+  public void addPlayer(final String guildId, final String player) {
+    map.put(guildId + PLAYER_LIST, map.get(PLAYER_LIST) + "," + player);
   }
 
-  public String getPlayers() {
-    return map.get(PLAYER_LIST);
+  public String getPlayers(final String guildId) {
+    return map.get(guildId + PLAYER_LIST);
   }
 }
