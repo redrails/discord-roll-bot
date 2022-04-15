0a33135036ddda6c3318ad87fe8613eb341db0bf
redrails <redrailz@gmail.com>
2022-04-15 02:28:03 +0100

    Fix code formatting

--- src/main/java/com/ihtasham/Listeners/ReactionListener.java
+++ src/main/java/com/ihtasham/Listeners/ReactionListener.java
@@ -77,9 +77,9 @@ public class ReactionListener extends ListenerAdapter {
       } else {
         for (int i = 0; i < sortedUsers.size(); i++) {
           sb.append(
-                  String.format(
-                          "%s: `%s` = `%s`\n",
-                          i + 1, sortedUsers.get(i).getName(), sortedUsers.get(i).getNumber()));
+              String.format(
+                  "%s: `%s` = `%s`\n",
+                  i + 1, sortedUsers.get(i).getName(), sortedUsers.get(i).getNumber()));
         }
 
         MessageUtils.sendMessage(message.getChannel(), sb.toString());
